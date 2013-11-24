5b39c17e462811684ce4865c4fc8febb62314423
Zex <top_zlynch@yahoo.com>
2013-11-24 22:04:52 +0800

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,42 @@
+/*
+ * Author: Zex <top_zlynch@yahoo.com>
+ *
+ * Basic for demo of SQLite C interfaces
+ *
+ * Copyright (c) by Zex Li <top_zlynch@yahoo.com>, Hytera Communications Co., Ltd.
+ *
+ */
+#include "basic.h"
+
+
+int general_cb(void* reserve, int argc, char** argv, char** colname)
+{
+	int i;
+
+	for (i = 0; i < argc; i++) 
+		printf("%s: %s\n", colname[i], argv[i]);
+	printf("\n");
+
+	return 0;
+}
+/*
+ * Author: Zex <top_zlynch@yahoo.com>
+ *
+ * Basic for demo of SQLite C interfaces
+ *
+ * Copyright (c) by Zex Li <top_zlynch@yahoo.com>, Hytera Communications Co., Ltd.
+ *
+ */
+#include "basic.h"
+
+
+int general_cb(void* reserve, int argc, char** argv, char** colname)
+{
+	int i;
+
+	for (i = 0; i < argc; i++) 
+		printf("%s: %s\n", colname[i], argv[i]);
+	printf("\n");
+
+	return 0;
+}
